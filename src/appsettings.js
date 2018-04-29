b6049e2eeca7e2efc7aa7267c41227bc0aaf59c0
aconfee <adamestela@gmail.com>
2018-04-28 19:45:50 -0500

    remove index of appsettings

--- src/appsettings.js
+++
@@ -1,3 +0,0 @@
-//export const API_ROOT = 'http://localhost:8080';
-export const API_ROOT = 'http://api.tinycrit.com'; // master branch
-//export const API_ROOT = 'http://api-dev.tinycrit.com'; // develop branch
