a3de2f32efe76acd33b1f6e61521df5fd8e4869e
aconfee <adamestela@gmail.com>
2018-05-01 13:27:44 -0500

    staging url

--- src/appsettings.js
+++ src/appsettings.js
@@ -1,3 +1,4 @@
 //export const API_ROOT = 'http://localhost:8080';
-export const API_ROOT = 'http://api.tinycrit.com'; // master branch
+//export const API_ROOT = 'http://api.tinycrit.com'; // master branch
 //export const API_ROOT = 'http://api-dev.tinycrit.com'; // develop branch
+export const API_ROOT = 'http://api-staging.tinycrit.com'; // staging branch
